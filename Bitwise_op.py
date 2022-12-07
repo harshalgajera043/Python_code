41e23e84db71632ce2d3e38f2c4dd5299a71821a
harshalgajera043 <83157209+harshalgajera043@users.noreply.github.com>
2022-12-07 15:21:24 +0530

    Add files via upload

---
+++ Bitwise_op.py
@@ -0,0 +1,19 @@
+a = 24
+b = 13
+c = a & b;
+print("value of c is ",c)
+
+c = a | b;
+print("Value of c is ",c)
+
+c = a ^ b;
+print("Value of c is ",c)
+
+c = ~a;
+print("Print value of c is ",c)
+
+c = a << 1;
+print("Print value of c is ",c)
+
+c = a >> 1;
+print("Print value of c is ",c)
