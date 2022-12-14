8c2920b9109877e3f6aaa905dfda03a1709d3fbe
harshalgajera043 <83157209+harshalgajera043@users.noreply.github.com>
2022-12-14 10:16:47 +0530

    Add files via upload

---
+++ porosity.py
@@ -0,0 +1,4 @@
+porosity = float(input("enter the formation porosity :"))
+permeability = float(input("enter:")
+print(f"formation porosity is",{porosity},and "permeability is ",{permeability})
+                     
